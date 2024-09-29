e3e70d91d841f90f4340a9d314aad00e520de192
eshaann <eshaann@nycstudents.net>
2024-09-28 20:51:00 -0400

    hi

--- index.js
+++ index.js
@@ -28,7 +28,34 @@ app.post("/uploadDoc/:fileName/:email", async (req, res) => {
     await File.add(fileData);
     res.send({ msg: "doc name uploaded" });
 });
+/*
+app.get("/getDocs/:email", async (req, res) => {
+    const { email } = req.params;
+    const { keywords } = req.query;
 
+    const keywordArray = keywords.split(',');
 
+});
+*/
+app.get("/getDocs/:email", (req, res) => {
+    const { email } = req.params; // Get the email from route params
+    const { keywords } = req.query; // Get the keywords from query params
 
+    if (!email) {
+        return res.status(400).send({ msg: "Email is required" });
+    }
+
+    if (!keywords) {
+        return res.status(400).send({ msg: "Keywords are required" });
+    }
+
+    // Split the keywords into an array if they are provided as a comma-separated string
+    const keywordArray = keywords.split(',');
+
+    res.send({
+        msg: "Data received",
+        email: email,
+        keywords: keywordArray,
+    });
+});
 app.listen(4000, () => console.log("up 4000"));
